81504f9cc2a68eed7c920975be2fe0d5dd55d109
Geethika-Amiti <147685854+Geethika-Amiti@users.noreply.github.com>
2025-05-11 16:03:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let pages = document.querySelectorAll('.page');
-let currentPage = 0;
-
-function turnPage() {
-  if (currentPage < pages.length - 1) {
-    pages[currentPage].style.transform = 'rotateY(180deg)';
-    currentPage++;
-    pages[currentPage].style.transform = 'rotateY(0deg)';
-  }
-}
-
-document.querySelector('.book').addEventListener('click', turnPage);
+window.addEventListener('DOMContentLoaded', () => {
+  $('#flipbook').turn({
+    width: 900,
+    height: 600,
+    autoCenter: true,
+    gradients: true,
+    duration: 1500
+  });
+});
